fe18dbcac2b55bbc9202f5d0bfbb618e5e72807a
JoseEduardoPizano <42656826+JoseEduardoPizano@users.noreply.github.com>
2022-08-31 19:44:17 -0500

    Add files via upload

--- consistent_hash/test.py
+++ consistent_hash/test.py
@@ -38,43 +38,60 @@ def run(words):
   to the store, the selected hash scheme is used to determine where to place
   the records.
   """
-  my_hash = CHash()
-  my_store = Store(my_hash)
+  my_hash_c = CHash()
+  my_store_c = Store(my_hash_c)
+  
+  my_hash_m = ModHash()
+  my_store_m = Store(my_hash_m)
   
   
   """
   Add three nodes to the Store
   """
-  my_store.add_node("Node 1")
-  my_store.add_node("Node 2")
-  my_store.add_node("Node 3")
+  my_store_c.add_node("Node 1")
+  my_store_c.add_node("Node 2")
+  my_store_c.add_node("Node 3")
   
-  my_store.dump()
+  my_store_c.dump()
+  
+  my_store_m.add_node("Node 1")
+  my_store_m.add_node("Node 2")
+  my_store_m.add_node("Node 3")
+  
+  my_store_m.dump()
   
 
   """
   Save all words in the Store
   """
   for word in words:
-      my_store.add_resource(word)
+      my_store_c.add_resource(word)
+      my_store_m.add_resource(word)
   
-  my_store.dump()
+  my_store_c.dump()
+  my_store_m.dump()
 
 
   """
   Remove one node from the Store. Stored objects need to be migrated to the
   remaining nodes.
   """
-  my_store.remove_node("Node 1")
-  my_store.dump()
+  my_store_c.remove_node("Node 1")
+  my_store_c.dump()
+  
+  my_store_m.remove_node("Node 1")
+  my_store_m.dump()
 
 
   """
   Add the node back to the Store. Objects need to be migrated to conform to the
   Hash scheme.
   """
-  my_store.add_node("Node 1")
-  my_store.dump()
+  my_store_c.add_node("Node 1")
+  my_store_c.dump()
+  
+  my_store_m.add_node("Node 1")
+  my_store_m.dump()
 
 
 
